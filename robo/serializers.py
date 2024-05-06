2b7d65e82308389b91bbb1d312e0bf7a021e14c5
Robobibek <pdlbibek706@gmail.com>
2024-05-06 13:54:26 +0545

    Django API with REST_framework

---
+++ robo/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import Studenta
+
+class StudentaSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=100)
+    roll = serializers.IntegerField()
+    city = serializers.CharField(max_length=100)
+
+    def create(self, validated_data):
+        return Studenta.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.roll = validated_data.get('roll', instance.roll)
+        instance.city = validated_data.get('city', instance.city)
+        instance.save()
+        return instance
+    
+
+# Field validation
+def validate_roll(self, value):
+    if value >= 200:
+        raise serializers.ValidationError('Seat Full')
+    return value
